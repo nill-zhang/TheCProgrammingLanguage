543e6aad6a5649d2d04a437ec1ac7f2e6d572fdb
sfzhang <andy.ledway3@gmail.com>
2017-04-06 19:23:33 -0400

    added WordPrint function

--- print.c
+++ print.c
@@ -2,8 +2,7 @@
 // Created by sfzhang 4/5/2017.
 //
 #include <stdio.h>
-
-void main(){
+void FmtPrint(){
     char c;
     int age;
     int a;
@@ -29,4 +28,46 @@ void main(){
     printf("Integer: %-8d\t%-8d\n",age,age); /*left alignment*/
     printf("Integer: %08d\t%08d\n",age,age); /*right alignment padded with 0s*/
     printf("Character Constant: %s\n",name);
+
+}
+
+/* WordPrint prints a text stream one word per line*/
+void WordPrint(){
+    char c;
+    char in[1000];
+    int l=0;
+    while((c = getchar())!= EOF){
+        in[i] = c;
+        l++;
+    }/*get all the input characters*/
+
+    int i;
+    char temp[500]; /*holding a word*/
+    int inword =0;
+    int j = 1; /* the first index of a word, or index of any whitespace character*/
+    for (i=0;input[i]!='\0';i++){
+
+        if (input[i]!='\t'||input[i]!=' '||input[i]!='\n'){
+            temp[i-j-1]=input[i];
+            inword = 1;
+        }else{
+            if (inword == 1){
+
+                inword = 0;
+                temp[i-j-1] = '\0';
+                for (int k=0;temp[k]!='\0';k++){
+                    printf("%c",temp[k]);
+
+                }
+                printf("\n");
+            }
+            j = i;
+        }
+
+    }
+
+}
+void main(){
+    WordPrint();
+
 }
