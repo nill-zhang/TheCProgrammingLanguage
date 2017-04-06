98143441c328f3978a9f017e751cfa89e3b413a5
sfzhang <andy.ledway3@gmail.com>
2017-04-06 01:22:45 -0400

    first commit

--- print.c
+++ print.c
@@ -5,19 +5,28 @@
 
 void main(){
     char c;
-    int i;
-    char name[];
+    int age;
+    int a;
+    char name[100];
     float f;
-    c = ' ';
-    i = 256;
-    name = "sfzhang";
-    f = 1.3244521;
+    c = '&';
+    age = 256;
+    for (int i=65;i<=75;i++){
+        name[i-65] = i;
+
+    }
+    f = 1.324452;
+    a = 98;
     printf("single character: %c\n",c);
+    printf("single  character: %c\n",a);
     printf("single character: %d\n",c);
-    printf("decimal: %d\n",i);
-    printf("Octal: %o\n",i);
-    printf("Hex: %x\n",i);
-    printf("Float: %3.f\n",f);
-    printf("Integer: %-3d\t%-3d\n",i,i);
+    printf("decimal(age): %d\n",age);
+    printf("Octal(age): %o\n",age);
+    printf("Hex(age): %x\n",age);
+    printf("Float: %3.f\n",f);               /* ignore fraction part*/
+    printf("Float: %3.2f\n",f);
+    printf("Float: %0.2f%%\n",f*100);        /*keep 2 digits after decimal point */
+    printf("Integer: %-8d\t%-8d\n",age,age); /*left alignment*/
+    printf("Integer: %08d\t%08d\n",age,age); /*right alignment padded with 0s*/
     printf("Character Constant: %s\n",name);
 }
