59ee60da0e41ef25c02d4dfbf711412d5bdcf730
sfzhang <andy.ledway3@gmail.com>
2017-04-12 23:59:42 -0400

    added comments

--- stack.cpp
+++ stack.cpp
@@ -5,12 +5,13 @@
 #define STACKSIZE 200
 
 char stackArray[STACKSIZE];
-char *StackPointer=stackArray; /*Points to the next available stack Position*/
+char *StackPointer = stackArray; /*Points to the next available stack Position*/
 
 void Push(char s);
 char Pop(void);
 //int isEmpty(void);
 
+/*Push add a new element to the stack*/
 void Push(char s) {
 
     if (StackPointer - stackArray >= STACKSIZE) {
@@ -26,7 +27,7 @@ void Push(char s) {
 
 
 
-
+/*Pop removes the top element from the stack*/
 char Pop(void){
     if (isEmpty()){
 
@@ -40,20 +41,20 @@ char Pop(void){
     }
 }
 
-
+/*isEmpty checks the stacks emptiness*/
 int isEmpty(void){
 
     return (StackPointer==stackArray) ? 1 : 0;
 }
 
-
+/*Len gives the length of the stack*/
 void Len(){
 
     return StackPointer - stackArray;
 
 }
 
-
+/*Peek returns the top element in the stack*/
 int Peek(){
 
     return (isEmpty())? -1:(*(StackPointer-1));
