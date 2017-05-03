96010d8b86e06ded1531c1e7726697f7ca2c733e
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-05-02 23:58:35 -0400

    some realloc traps

--- memory.c
+++ memory.c
@@ -11,7 +11,11 @@
 #define DEBUG
 
 flag malloc_help(void **, size_t);
+void malloc_helper_test(void);
 void freeMem(void *);
+void freeMem_test(const char *);
+void realloc_test(void);
+flag realloc_helper(void **, size_t);
 
 
 /*
@@ -20,7 +24,6 @@ void freeMem(void *);
  * and save the allocated pointer(*p) as the contents of pv
  * thus, the contents(pc) of the real argument(&pc) is set.
  */
-
  flag malloc_help(void **pv, size_t s){
     char **p = (char **)pv;         // type conversion
     assert(pv != _NULL && s != 0);  // evaluate arguments
@@ -32,6 +35,21 @@ void freeMem(void *);
     #endif
     return (*p != _NULL);
 }
+
+flag realloc_helper(void **pv, size_t s){
+    char **pch = (char **)pv;      // cast to our type
+    char *pNew;
+    pNew = realloc(*pch, s);
+    if (pNew != _NULL){
+        printf("pNew not null\n");
+        *pch = pNew;               // if realloc succeed, redirect the old pointer
+        memset(pNew, 0xcc, s);
+    }
+
+    return (pNew != _NULL);
+
+}
+
 
 void freeMem(void *pv){
 
@@ -50,15 +68,64 @@ void freeMem_test(const char *pcc){
     }
 }
 
+void realloc_test(void){
 
-int main(void){
+    // Note: the following commented pointer can not be passed to realloc,
+    // they will cause undefined behavior
+    // realloc only takes pointers returned by dynamic mem functions such as
+    // realloc, malloc, calloc,etc.
+    // char *pwho1 = "I am sfzhang";
+    // char name[] = {'I',' ','a','m',' ','s','f','z','h','a','n','g',','};
+    // char *pwho1 = name;
+    char name[]= "I am sfzhang,";
+    char *pwho1 = malloc(strlen(name)+1);
+    char *pwho2 = malloc(strlen(name)+1);
+    if (pwho1 != _NULL){
+        strcpy(pwho1, name);
+    }
 
-    freeMem_test("my name is sfzhang!!\n");
+    if (pwho2 != _NULL){
+        strcpy(pwho2, name);
+    }
+
+    char *pwhere = "I live in Toronto!";
+    char *pWhole;
+    char *pPart;
+    printf("old pwho1 address:%p, contents:%s\n",pwho1,pwho1);
+    printf("old pwho2 address:%p, contents:%s\n",pwho2,pwho2);
+    pPart = realloc(pwho1,3);
+    pWhole = realloc(pwho2, strlen(pwhere)+strlen(pwho2)+1);
+    printf("new pwho1 address:%p, contents:%s  pPart(len:):%p, %s\n",pwho1,pwho1,pPart,pPart);
+    printf("new pwho2 address:%p, contents:%s  pWhole:%p, %s\n",pwho2,pwho2,pWhole,pWhole);
+
+}
+
+
+void realloc_helper_test(void){
+    char *pfirstname = "shaofeng";
+    char *plastname = "zhang";
+    unsigned int status;
+    status = realloc_helper((void **)&pfirstname,strlen(pfirstname)+strlen(plastname)+1);
+    printf(status?"reallocation succeeded!":"reallocation failed!");
+    printf("now firstname %s\n",pfirstname);
+    strcpy(pfirstname,"shaofeng zhang");
+}
+
+
+void malloc_helper_test(void){
     unsigned int status;
     char *pc=_NULL;
     status = malloc_help((void *)&pc, 10);
     printf(status?"successful allocation\n":"failed allocation!\n");
 
+}
+
+int main(void){
+
+    // freeMem_test("my name is sfzhang!!\n");
+    // malloc_helper_test();
+    // realloc_helper_test();
+    realloc_test();
 
 
 }
