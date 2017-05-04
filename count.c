54523f8ca6583385931987b0e7d703ca56d5f953
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-05-04 02:53:44 -0400

    fix bugs and run test

--- count.c
+++ count.c
@@ -59,7 +59,6 @@ void count() {
         }
     }
 
-
     if (inword == 1) {
         nword++;
     }/*if I sent EOF while I am still in a word, I need to count this word in*/
@@ -74,7 +73,7 @@ void count() {
 }
 void getWordLengths(unsigned *plengths){
     int chr;
-    int len;
+    int len =0;
     while ((chr = getchar())!= EOF){
         if (chr == '\n'||chr== '\t'|| chr ==' '){
             if (len != 0){
@@ -87,21 +86,16 @@ void getWordLengths(unsigned *plengths){
         }else{
             len++;
         }
-
-
-
     }
-
-
-
-
 }
 
-int maximum(unsigned *plengths, int s){
+unsigned maximum(unsigned *plengths, int s){
     int i;
-    int max = -1;
+    unsigned max = 0;
     for(i=0;i<s;i++){
-        if (plengths[i] > max){
+        if (plengths[i] > max){ /* compare unsigned *plengths with signed max which is -1
+                                 * -1 is all 1s in two's complement representation, the condition
+                                 * will always be false*/
             max = plengths[i];
         }
     }
@@ -111,43 +105,44 @@ int maximum(unsigned *plengths, int s){
 }
 
 void wordlength_frequency_histogram(char mark){
-    unsigned lengths[MAX_WORD_LENGTH]; /* lengths array saves word length frequencies according to their
-                                          corresponding indices, lengths[i] has the frequency of words with
-                                          length i */
-    int i,j,k,max;
+    unsigned lengths[MAX_WORD_LENGTH], max; /* lengths array saves word length frequencies according to their
+                                             * corresponding indices, lengths[i] has the frequency of words with
+                                             * length i */
+    int i,j,k;
     for(i=0;i < MAX_WORD_LENGTH;i++){
         lengths[i] = 0;
     }
 
     getWordLengths(lengths);
     max = maximum(lengths,MAX_WORD_LENGTH);
-    for (i=0;i<max;i++){
-        for (j=0;j<MAX_WORD_LENGTH;j++){
+    printf("\n\nfreq\n\n");
+    for (i=0; i < max; i++){
+        printf("%*d\t",4,max-i);           /*max-width is 4, right alignment*/
+        for (j=0; j < MAX_WORD_LENGTH; j++){
             if (lengths[j] == 0){
                 continue;
-            }else if(lengths[j]+i>=max){
+            }else if((lengths[j]+i) >= max){
                 printf("%c ",mark);
             }else{
                 printf("  ");
+            }
+
+            if ((j/10) > 0){
+                printf(" ");       /* adjust alignment for double digit word length*/
             }
         }
         printf("\n");
 
     }
+    printf("   0\t");
 
-    for(k=0;k<MAX_WORD_LENGTH;k++){
+    for(k=0; k < MAX_WORD_LENGTH; k++){
         if(lengths[k] != 0){
             printf("%d ",k);
         }
-
     }
-    printf("\n");
 
-
-
-
-
-
+    printf("  word length\n");
 }
 
 
@@ -158,3 +153,21 @@ int main(){
 
 
 }
+
+/* Result:
+ *
+ * freq
+ *   12      -
+ *   11      -
+ *   10      -
+ *    9      -
+ *    8      -
+ *    7      - -
+ *    6      - -
+ *    5      - -       -   -
+ *    4    - - - -   - - - -
+ *    3    - - - - - - - - -          -
+ *    2    - - - - - - - - -          -
+ *    1    - - - - - - - - - -  -  -  -  -  -  -
+ *    0    1 2 3 4 5 6 7 8 9 10 11 12 13 15 17 21   word length
+ */
